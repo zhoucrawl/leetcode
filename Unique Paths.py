24951e5f9b043df3ca462cab85674ebca7235765
zhoucrawl <545761959@qq.com>
2018-04-24 11:27:33 +0800

    Create Unique Paths.py

---
+++ Unique Paths.py
@@ -0,0 +1,11 @@
+
+int uniquePaths(int m, int n) {  
+        if(m<1 || n<1) return 0;  
+        if(m==1 && n==1) return 1;  
+        return uniquePaths(m-1,n)+uniquePaths(m,n-1);  
+    } 
+class Solution:    
+    def uniquePaths(int m,intn):
+        if (m<1 || n<1): return 0
+        if (m==1 && n==1):return 1
+        return uniquepPaths(m-1,n)+uniquePaths(m,n-1);
