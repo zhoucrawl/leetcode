3a481bb09e829fd93c1b5688247ccfbb6b16fb21
zhoucrawl <545761959@qq.com>
2018-05-09 16:27:03 +0800

    Update Unique Paths.py

--- Unique Paths.py
+++ Unique Paths.py
@@ -22,3 +22,17 @@ class Solution:
             for j in range(1,n):
                 tmp[i][j]= tmp[i-1][j]+tmp[i][j-1]
         return tmp[-1][-1]
+
+ 
+#有障碍的解法   
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        P = [[0 for j in range(n+1)] for i in range(m+1)]
+        
+        P[0][1] = 1
+        
+        for i in range(1,m+1):
+            for j in range(1, n+1):
+                if obstacleGrid[i-1][j-1] != 1:
+                    P[i][j] = P[i-1][j] + P[i][j-1]
+        return P[m][n]
