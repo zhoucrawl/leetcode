a143e37688cfc345333b35b5b037596b688912b4
zhoucrawl <545761959@qq.com>
2018-08-07 13:54:08 +0800

    Update maximum_subarry.py

--- maximum_subarry.py
+++ maximum_subarry.py
@@ -1,5 +1,26 @@
 
-1.给定一个数组，找出连续子串的最大值。
+#1.给定一个数组，找出连续子串的最大值。
+#方法一，没有使用内置函数，减少了时间
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max = nums[0]
+        sum = 0
+        
+        for idx in range(len(nums)):
+            if sum < 0:
+                sum = 0  
+            #每次加的为大于等于0的数    
+            sum += nums[idx]
+            if max < sum:
+                max = sum
+                
+        return max
+
 
 class Solution:
     def maxSubArray(self, nums):
