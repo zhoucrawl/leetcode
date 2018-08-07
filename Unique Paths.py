2d0903f64d2f6809eff5ea2ded5a49c1ff8f8d24
zhoucrawl <545761959@qq.com>
2018-08-07 14:31:15 +0800

    Update Unique Paths.py

--- Unique Paths.py
+++ Unique Paths.py
@@ -1,10 +1,17 @@
 
 #分治法
-class Solution:    
-    def uniquePaths(int m,intn):
-        if (m<1 or n<1): return 0
-        if (m==1 and n==1):return 1
-        return uniquepPaths(m-1,n)+uniquePaths(m,n-1);
+class Solution(object):
+    def uniquePaths(self,m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m < 1 and n < 1:
+            return 0
+        if m==1 and n==1:
+            return 1
+        return self.uniquePaths(m-1,n)+self.uniquePaths(m,n-1)
 
 #动态规划
 class Solution:
